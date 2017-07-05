134cfdc5c001bb4f53f94b2cb94b400724aab853
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-07-05 16:42:53 -0500

    main controller checks location.url()

--- public/scripts/dev/controllers/mainController.js
+++ public/scripts/dev/controllers/mainController.js
@@ -1,3 +1,19 @@
-app.controller('mainController', function() {
+app.controller('mainController', function($location) {
   console.log('mainController');
+  let vm = this;
+
+  //display nav bar boolean
+  vm.showNavBar = false;
+
+  //watches current location and sets vm.showNavBar accordingly
+  vm.checkCurrentLocation = function() {
+    console.log($location.url());
+    if ($location.url() == '/' || $location.url() == '/register') {
+      vm.showNavBar = false;
+    } else {
+      vm.showNavBar = true;
+    }
+  }; //end checkCurrentLocation()
+
+  //read local storage to see if user is logged in
 });
---
+++ public/scripts/dev/services/go.js
@@ -0,0 +1,8 @@
+app.service('go', function($location) {
+  let vm = this;
+
+  //takes a url and changes $location
+  vm.to = function(url) {
+    $location.url();
+  }; // end to
+});
