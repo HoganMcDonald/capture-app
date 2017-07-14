5938af12997af269a3fe6eadf17015c1a4024118
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-07-14 16:39:34 -0500

    ready to try deploying?

--- modules/db.js
+++ modules/db.js
@@ -2,12 +2,33 @@
 const pg = require('pg');
 
 //globals
-const config = {
-  database: 'capture',
-  host: 'localhost',
-  port: 5432,
-  max: 20
-};
+
+if (process.env.DATABASE_URL) {
+  var params = url.parse(process.env.DATABASE_URL);
+  var auth = params.auth.split(':');
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+
+    ssl: true, // heroku requires ssl to be true
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
+
+} else {
+
+  const config = {
+    database: 'capture',
+    host: 'localhost',
+    port: 5432,
+    max: 20
+  };
+
+}
 
 const pool = new pg.Pool(config);
 
