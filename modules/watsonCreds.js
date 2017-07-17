ce2bc4fa004964d081d2c345623d7f8a9988b114
Hogan McDonald <hpeter.mcdonald@icloud.com>
2017-07-16 20:59:31 -0500

    branch up to date with deployment

--- modules/watsonCreds.js
+++ modules/watsonCreds.js
@@ -1,7 +1,9 @@
 //credential object
 const watsonCreds = {
-  username: 'f5399988-c52d-4ad2-ae21-6bfcd254ef0a',
-  password: 'B4yG0cYtUMTG'
+  // username: 'f5399988-c52d-4ad2-ae21-6bfcd254ef0a', //gmail
+  username: '350b8f18-8c80-4c26-8312-70fa24308b7e', //icloud
+  // password: 'B4yG0cYtUMTG' //gmail
+  password: '7iUjrJ5we6ML'
 };
 
 module.exports = watsonCreds;
